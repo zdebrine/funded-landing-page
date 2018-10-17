f62aa53c6cbfb0b104c53c060d8685264bacb962
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2018-10-17 11:36:52 -0700

    Add initial analytics to landing and pricing pages Landing Page: - Click Header GitHub icon - Click Header Dashboard button - Click Documentation button - Click Create geofence button - Hover over code area - Click on code area - Click Get started description links - Click modal tutorial video
    Pricing Page:
    - Hover pricing cards
    - Clicked one of the pricing buttons
    - Hovered over pricing insight cards

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,11 +1,61 @@
-$("#open-getting-started-video").click(function() {
-    analytics.track("Opened getting started modal window");
-})
+// Landing page analytics
 
-$("#github-icon").click(function() {
+$("#header-github-icon").click(function() {
     analytics.track("Clicked on GitHub icon");
 })
 
-$("#dashboard-header").click(function() {
+$("#header-dashboard-button").click(function() {
     analytics.track("Clicked on dashboard in header");
+})
+
+$("#documentation-button").click(function() {
+    analytics.track("Clicked on documentation button");
+    console.log("Entered doc button");
+})
+
+$("#create-geofence-button").click(function() {
+    analytics.track("Clicked on create geofence button");
+    console.log("Entered geofence button");
+})
+
+$("#code-analytics").hover(function() {
+    analytics.track("Hovered over code area");
+})
+
+$("#code-analytics").click(function() {
+    analytics.track("Clicked on code area");
+})
+
+$("#get-started-description").click(function() {
+    analytics.track("Clicked a link in the get started description");
+})
+
+$("#getting-started-video").click(function() {
+    analytics.track("Opened getting started modal window");
+})
+
+// Pricing page analytics
+
+$("#pricing-cards").hover(function() {
+    analytics.track("Hovered over pricing cards");
+})
+
+$("#pricing-free-button").click(function() {
+    analytics.track("Clicked on free sign up button");
+})
+
+$("#pricing-basic-button").click(function() {
+    analytics.track("Clicked on basic buy button");
+})
+
+$("#pricing-startup-button").click(function() {
+    analytics.track("Clicked on startup buy button");
+})
+
+$("#pricing-enterprise-button").click(function() {
+    analytics.track("Clicked on enterprise buy button");
+})
+
+$("#pricing-insight-cards").hover(function() {
+    analytics.track("Hovered over pricing insight cards");
 })
